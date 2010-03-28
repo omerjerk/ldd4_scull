2ab1c876015f29c1ea38558fc244e5ad54415804
dyno.fu <dyno.fu@9bbf9853-338b-a0da-49c6-d86b26529a97>
2010-03-28 15:15:38 +0000

    ch3: introduce current_cred()

--- trunk/scull/access.c
+++ trunk/scull/access.c
@@ -29,7 +29,21 @@
 #include <asm/atomic.h>
 #include <linux/list.h>
 
+#include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27)
+#include <linux/sched.h>
+#include <linux/cred.h>
+#endif
+
 #include "scull.h"        /* local definitions */
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(2, 6, 27)
+#define CURRENT_UID current->uid
+#define CURRENT_EUID current->euid
+#else
+#define CURRENT_UID current_cred()->uid
+#define CURRENT_EUID current_cred()->euid
+#endif
 
 static dev_t scull_a_firstdev;  /* Where our range begins */
 
@@ -103,15 +117,15 @@ static int scull_u_open(struct inode *inode, struct file *filp)
 
 	spin_lock(&scull_u_lock);
 	if (scull_u_count && 
-			(scull_u_owner != current->uid) &&  /* allow user */
-			(scull_u_owner != current->euid) && /* allow whoever did su */
+			(scull_u_owner != CURRENT_UID) &&  /* allow user */
+			(scull_u_owner != CURRENT_EUID) && /* allow whoever did su */
 			!capable(CAP_DAC_OVERRIDE)) { /* still allow root */
 		spin_unlock(&scull_u_lock);
 		return -EBUSY;   /* -EPERM would confuse the user */
 	}
 
 	if (scull_u_count == 0)
-		scull_u_owner = current->uid; /* grab it */
+		scull_u_owner = CURRENT_UID; /* grab it */
 
 	scull_u_count++;
 	spin_unlock(&scull_u_lock);
@@ -162,8 +176,8 @@ static spinlock_t scull_w_lock = SPIN_LOCK_UNLOCKED;
 static inline int scull_w_available(void)
 {
 	return scull_w_count == 0 ||
-		scull_w_owner == current->uid ||
-		scull_w_owner == current->euid ||
+		scull_w_owner == CURRENT_UID ||
+		scull_w_owner == CURRENT_EUID ||
 		capable(CAP_DAC_OVERRIDE);
 }
 
@@ -181,7 +195,7 @@ static int scull_w_open(struct inode *inode, struct file *filp)
 		spin_lock(&scull_w_lock);
 	}
 	if (scull_w_count == 0)
-		scull_w_owner = current->uid; /* grab it */
+		scull_w_owner = CURRENT_UID; /* grab it */
 	scull_w_count++;
 	spin_unlock(&scull_w_lock);
 
